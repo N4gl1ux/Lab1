394cf7a07f938dfff261688cef121dca359a554d
N4gl1ux <nagliuxpro@gmail.com>
2023-09-12 20:05:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvMain = (TextView)findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view){
+
+        this.tvMain.setText("Labas, linkiu grazios dienos!");
+    }
+}
