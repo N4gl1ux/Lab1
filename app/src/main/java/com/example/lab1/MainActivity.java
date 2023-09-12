cb0e562181bda6d9850840933eb247b5df0def59
N4gl1ux <nagliuxpro@gmail.com>
2023-09-12 20:57:17 +0300

    Added text color changing button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvMain;
+    private TextView textColorChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = (TextView)findViewById(R.id.tvMain);
+        this.textColorChange = (TextView)findViewById(R.id.textColorChange);
     }
 
     public void onBtnChangeTextClick(View view){
 
         this.tvMain.setText("Labas, linkiu grazios dienos!");
+    }
+
+    public void onBtnChangeTextColor(View view){
+
+        this.textColorChange.setTextColor(Color.RED);
     }
 }
